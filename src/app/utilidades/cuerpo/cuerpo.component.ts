c67912d00449c177b1860f3390eb3df48ced96b2
ccastillo_tegik <ccastillo@tegik.com>
2020-02-20 00:14:20 -0600

    colocando un poco de estilo a los botones y los inputs

--- src/app/utilidades/cuerpo/cuerpo.component.ts
+++ src/app/utilidades/cuerpo/cuerpo.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-cuerpo',
@@ -7,11 +7,10 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CuerpoComponent implements OnInit {
 
-	descripcion: string="Estas es las descripcion del producto";
+  @Input() descripcion: string = "Estas es las descripcion del producto";
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit() {
-  }
+  ngOnInit() {}
 
 }
--- src/app/utilidades/pie/pie.component.ts
+++ src/app/utilidades/pie/pie.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Output, EventEmitter } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter, Input } from '@angular/core';
 
 @Component({
   selector: 'app-pie',
@@ -7,47 +7,47 @@ import { Component, OnInit, Output, EventEmitter } from '@angular/core';
 })
 export class PieComponent implements OnInit {
 
-  precio: number = 12.33;
-  cantidad: number=1;
+  @Input() precio: number = 12.33;
+  cantidad: number = 1;
 
-  importe: number=0;
+  importe: number = 0;
 
-  @Output('obtenerImporte') onImporte: EventEmitter<number>= new EventEmitter<number>();
+  @Output('obtenerImporte') onImporte: EventEmitter < number >= new EventEmitter < number > ();
 
-  @Output() onSeleccionar: EventEmitter<boolean>= new EventEmitter<boolean>();
+  @Output() onSeleccionar: EventEmitter < boolean >= new EventEmitter < boolean > ();
 
   constructor() {}
 
   ngOnInit() {
 
-  	/*
+    /*
 sskfjsdfkjskfjsdkdfj
-  	*/
+    
 
 
     setTimeout(() => {
       this.precio = 15.33;
 
     }, 3000);
-
+*/
 
 
   }
 
-  calculoImporte(event: any){
-  	console.log(event);
-  	this.importe = event.target.value*1.16;
+  calculoImporte(event: any) {
+    console.log(event);
+    this.importe = event.target.value * 1.16;
 
   }
 
-  calculaTotal(){
-  	//alert("Ya estoy aqui");
-  	let total = this.importe* this.cantidad;
-  	this.onImporte.emit(total);
+  calculaTotal() {
+    //alert("Ya estoy aqui");
+    let total = this.importe * this.cantidad;
+    this.onImporte.emit(total);
   }
 
-  seleccionar(){
-    
+  seleccionar() {
+
     this.onSeleccionar.emit(true);
 
   }
