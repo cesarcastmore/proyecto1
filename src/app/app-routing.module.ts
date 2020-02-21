d99408179545b918b668e9df70976187509344e2
ccastillo_tegik <ccastillo@tegik.com>
2020-02-20 19:58:59 -0600

    directives en el root

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,42 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { VistaTarjetaComponent } from './paginas/vista-tarjeta/vista-tarjeta.component';
+import { VistaBootstrapComponent } from './paginas/vista-bootstrap/vista-bootstrap.component';
+import { VistaCondiccionComponent } from './paginas/vista-condiccion/vista-condiccion.component';
+import { VistaCicloComponent } from './paginas/vista-ciclo/vista-ciclo.component';
+import { VistaClaseComponent } from './paginas/vista-clase/vista-clase.component';
+import { VistaEstiloComponent } from './paginas/vista-estilo/vista-estilo.component';
+import { ModulosExternosComponent } from './paginas/modulos-externos/modulos-externos.component';
+import { VistaSwitchComponent } from './paginas/vista-switch/vista-switch.component';
 
-
-const routes: Routes = [];
+const routes: Routes = [{
+  path: 'componentes/componentes',
+  component: VistaTarjetaComponent
+}, {
+  path: 'modulos/bootstrap',
+  component: VistaBootstrapComponent
+}, {
+  path: 'directivas/condiccion',
+  component: VistaCondiccionComponent
+}, {
+  path: 'directivas/ciclo',
+  component: VistaCicloComponent
+}, {
+  path: 'directivas/clase',
+  component: VistaClaseComponent
+}, {
+  path: 'directivas/estilo',
+  component: VistaEstiloComponent
+}, {
+  path: 'modulos/modulos_externos',
+  component: ModulosExternosComponent
+}, {
+  path: 'directivas/switch',
+  component: VistaSwitchComponent
+}];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
